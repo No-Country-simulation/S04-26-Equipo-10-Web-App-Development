22c759d16ffa80aca8d08c2f8f482224a37ce348
Rocio Guadalupe Manquillan <guadalupemanquillan@gmail.com>
2026-05-13 14:57:39 -0300

    fix: remove priorities route from MVP catalogs

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,6 @@ import incidentsRoutes from "./modules/incidents/incidents.routes.js"
 import authRoutes from "./modules/auth/auth.routes.js"
 import areasRoutes from "./modules/areas/areas.routes.js"
 import incidentTypesRoutes from "./modules/incident-types/incident-types.routes.js"
-import prioritiesRoutes from "./modules/priorities/priorities.routes.js"
 import statusesRoutes from "./modules/statuses/statuses.routes.js"
 import rolesRoutes from "./modules/roles/roles.routes.js"
 
@@ -15,7 +14,6 @@ router.use("/incidents", incidentsRoutes)
 router.use("/auth", authRoutes)
 router.use("/catalogs/areas", areasRoutes)
 router.use("/catalogs/incident-types", incidentTypesRoutes)
-router.use("/catalogs/priorities", prioritiesRoutes)
 router.use("/catalogs/statuses", statusesRoutes)
 router.use("/catalogs/roles", rolesRoutes)
 
